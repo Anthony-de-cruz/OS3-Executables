372731fa90c906c30a8a4fb0aebe29ae149d8e42
Anthony de Cruz <anthonydecruz@proton.me>
2023-12-03 15:14:05 +0000

    Feat: Parse arguments

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A C program to find executable files in a unix file system.
 
 
 Todo:
-- [ ] Parse argument directories both in the form of {dir1:dir2} in the style of PATH and {dir1 dir2}
+- [x] Parse argument directories both in the form of {dir1:dir2} in the style of PATH and {dir1 dir2}
 - [ ] Find all of the files within those directories
 - [ ] Identify whether or not they are executables via the inode using `stat`
 - [ ] Print it all out.
--- src/main.c
+++ src/main.c
@@ -2,17 +2,40 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 // Iterate over the directories,
-// printing all of the executable files found inside
+// printing all of the executable files found inside.
 //
-// returns true if all directories are valid
-// return false if there is an invalid directory
+// - returns true if all directories are valid
+// - return false if there is an invalid directory
 bool print_executables(char *directories[]) { return true; }
 
-// Parse argv into args
-void parse_args(int argc, char *argv[], char *args[]) {}
+// Parse argv into args.
+//
+// - Accepts formats ['/usr/bin', 'usr/sbin'] and ['/usr/bin:/usr/sbin'].
+void parse_args(int argc, char *argv[], char *args[]) {
+
+    int dir = 0;
+    int arg = 0;
+
+    for (; arg + 1 < argc; arg++) {
+
+        char *temp = strdup(argv[arg + 1]);
+        char *token = strtok(temp, ":");
+        
+        // Tokenise each string, delimiting by :
+        // and appending to args[]
+        while (token != NULL) {
+            args[dir] = token;
+            dir++;
+            token = strtok(NULL, ":");
+        }
+
+        free(token);
+    }
+}
 
 int main(int argc, char *argv[]) {
 
@@ -24,6 +47,12 @@ int main(int argc, char *argv[]) {
     char *args[ARG_MAX];
 
     parse_args(argc, argv, args);
+    
+    /*
+    printf("\nArgs check\n");
+    for (int x = 0; x < 15; x++) {
+        printf("    args[%d] = %s\n", x, args[x]);
+    }*/
 
     if (print_executables(args)) {
         return EXIT_SUCCESS;
