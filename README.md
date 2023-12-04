1fee597244e2a2f876a24206706826a2f97397e4
Anthony de Cruz <anthonydecruz@proton.me>
2023-12-04 00:17:49 +0000

    Feat: print out executables

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A C program to find executable files in a unix file system.
 Todo:
 - [x] Parse argument directories both in the form of {dir1:dir2} in the style of PATH and {dir1 dir2}
 - [x] Find all of the files within those directories
-- [ ] Identify whether or not they are executables via the inode using `stat`
-- [ ] Print it all out.
+- [x] Identify whether or not they are executables via the inode using `stat`
+- [x] Print it all out.
--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <dirent.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
-
-#include <linux/limits.h>
 
 // Parse argv into dirs.
 //
@@ -36,26 +36,73 @@ int parse_args(int argc, char *argv[], char **dirs) {
     return dirc;
 }
 
+// Checks whether or not the user has execution permissions for the file
+// at path.
+//
+// - Returns bool
+bool has_exe_perms(char *path) {
+
+    // Get file stat
+    struct stat stat_buf;
+    if (stat(path, &stat_buf) != 0) {
+        printf("%s\n", path);
+        perror("Stat error");
+        return false;
+    }
+
+    // The 7th bit of file_stat.st_mode signifies the execution permissions
+    // of the file. S_IXUSR acts as a bit mask for the 7th bit, allowing us
+    // to check it. S_ISREG() is a macro to check if it is regular file
+    if (stat_buf.st_mode & S_IXUSR && S_ISREG(stat_buf.st_mode)) {
+        return true;
+    }
+    return false;
+}
+
 // Iterate over the directories,
 // printing all of the executable files found inside.
 //
-// - Returns true if all directories are valid
-// - Returns false if there is an invalid directory
+// - Returns true if all directories are valid and
+//   false if there is an invalid directory
 bool print_executables(int dirc, char *dirs[]) {
 
-    for (int dir = 0; dir + 1 < dirc; dir++) {
-        printf("%s\n", dirs[dir]);
+    for (int x = 0; x + 1 <= dirc; x++) {
+        printf("DIRECTORY: %s\n", dirs[x]);
 
-        DIR *dp = opendir(dirs[dir]);
+        // Open the directory
+        DIR *dir = opendir(dirs[x]);
+        if (dir == NULL) {
+            perror("Error opening directory");
+            return false;
+        }
         struct dirent *entry;
 
-        while ((entry = readdir(dp)) != NULL) {
+        // Iterate over all the files
+        while ((entry = readdir(dir)) != NULL) {
+            
+            // Put together a path for the file
+            char *file_path = strdup(dirs[x]);
+            file_path = realloc(file_path, PATH_MAX);
+            strcat(file_path, "/");
+            strcat(file_path, entry->d_name);
 
-            printf("%8ld - %s\n", entry->d_ino, entry->d_name);
+            // Check if the file is executable
+            if (has_exe_perms(file_path)) {
+                printf("    %8ld - %20s\n", entry->d_ino, entry->d_name);
+            }
+
+            free(file_path);
         }
 
-        closedir(dp);
+        closedir(dir);
     }
+
+    /*
+    // DEBUG
+    printf("\nArgs check\n");
+    for (int x = 0; x < 15; x++) {
+        printf("    args[%d] = %s\n", x, dirs[x]);
+    }*/
 
     return true;
 }
@@ -70,12 +117,6 @@ int main(int argc, char *argv[]) {
     char **dirs = malloc(_POSIX_ARG_MAX * PATH_MAX);
 
     int dirc = parse_args(argc, argv, dirs);
-
-    /*
-    printf("\nArgs check\n");
-    for (int x = 0; x < 15; x++) {
-        printf("    args[%d] = %s\n", x, args[x]);
-    }*/
 
     if (print_executables(dirc, dirs)) {
         return EXIT_SUCCESS;
